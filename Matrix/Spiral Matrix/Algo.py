e5547ba0465fc370dcdee36be23854f41b5bd44b
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2025-05-16 14:43:48 +0500

    Fix matrix of images and show in spiral

---
+++ Matrix/Spiral Matrix/Algo.py
@@ -0,0 +1,36 @@
+class Solution:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        if not matrix or not matrix[0]:
+            return []
+
+        m, n = len(matrix), len(matrix[0])
+        result = []
+        top, bottom = 0, m - 1
+        left, right = 0, n - 1
+
+        while top <= bottom and left <= right:
+            for j in range(left, right + 1):
+                result.append(matrix[top][j])
+            top += 1
+
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom:
+                for j in range(right, left - 1, -1):
+                    result.append(matrix[bottom][j])
+                bottom -= 1
+
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+
+        return result
+
+
+# Test case
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+S = Solution()
+print(S.spiralOrder(matrix))
